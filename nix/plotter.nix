95faf4d791f4ee0086a1d99cdef1373b8b5ce3ef
Hydn <marti.llinas.comas@gmail.com>
2025-02-05 21:05:52 +0100

    f

--- nix/plotter.nix
+++ nix/plotter.nix
@@ -9,18 +9,24 @@
     dream2nix.modules.dream2nix.WIP-python-pdm
   ];
 
-  deps = {nixpkgs, ...}: {
-    pdm = nixpkgs.pdm;
-  };
+#  deps = {nixpkgs, ...}: {
+#    pdm = nixpkgs.pdm;
+#  };
 
   pdm.lockfile = ../python/pdm.lock;
   pdm.pyproject = ../python/pyproject.toml;
   #pdm.pythonInterpreter = config.deps.python3;
 
   mkDerivation = {
-    src = ./..;
+    src = lib.cleanSource ./..;
     buildInputs = [
       config.deps.python3.pkgs.pdm-backend
+    ];
+  };
+
+  buildPythonPackage = {
+    pythonImportsCheck = [
+      "plotter"
     ];
   };
 }
